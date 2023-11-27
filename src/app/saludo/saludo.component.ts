974a725dfa43749b24d4cd8cbb5bde310e293a24
noraarellano <72581638+noraarellano@users.noreply.github.com>
2023-11-27 00:07:05 -0600

    Cambio saludo

---
+++ src/app/saludo/saludo.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-saludo',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './saludo.component.html',
+  styleUrl: './saludo.component.css'
+})
+export class SaludoComponent {
+
+}
